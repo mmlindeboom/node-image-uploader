1be357500864965be2033ac80cf8d84cee8364a7
Matt Lindeboom <matt.lindeboom@gmail.com>
2013-03-04 13:09:53 -0500

    Added working request handler

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require("http");
+var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for" + pathname + " received");
 
-		route(pathname);
+		route(handle, pathname);
 
 		response.writeHead(200, {"Content-type": "text/plain"});
 		response.write("Hello World");
-		reponse.end();
+		response.end();
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
