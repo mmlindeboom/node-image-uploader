2a8757244d32175156bd91670f0dae8f66b4ec9e
Matt Lindeboom <matt.lindeboom@gmail.com>
2013-03-04 18:17:16 -0500

    Complete tutorial

--- requesthandlers.js
+++ requesthandlers.js
@@ -1,9 +1,31 @@
-function start() {
+var querystring = require("querystring");
+
+function start(response, postData) {
 	console.log("Request handler 'start' was called.");
+	var body = '<html>' +
+	'<head>' +
+	'<meta http-equiv="Content-type" content="text/html;' +
+	'charset=UTF-8" />' +
+	'</head>' +
+	'<body>' +
+	'<form action="/upload" method="post">'+
+		'<textarea name="foo" id="foo" cols="60" rows="20"></textarea>' +
+		'<br><input type="submit" value="Submit text" />' +
+	'</form>' +
+	'</body>' +
+	'</html>';
+
+	response.writeHead(200, {"Content-type": "text/html"});
+	response.write(body);
+	response.end();
 }
 
-function upload() {
+
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
+	response.writeHead(200, {"Content-type": "text/plain"});
+	response.write("You've sent the text: " + querystring.parse(postData).foo);
+	response.end();
 }
 
 exports.start = start;
